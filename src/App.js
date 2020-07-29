d1c04bcad614c7b8c2e76f2232955cab58b29021
nbogie <neillbogie@googlemail.com>
2020-07-29 01:09:58 +0100

    update app.js - link to repl.it project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <Palettes />
+      Make your own version of this app, starting at 
+      <a href="https://repl.it/@NeillBogie/nice-colours">https://repl.it/@NeillBogie/nice-colours</a>
     </div>
   );
 }
